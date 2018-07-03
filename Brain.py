8d611cae5f49b978ecf758a0bea4826be95d7651
rjhosler <40174069+rjhosler@users.noreply.github.com>
2018-07-03 12:54:57 -0400

    Post uses wasserstein clustering

--- Brain.py
+++ Brain.py
@@ -24,102 +24,6 @@ import PointProcess
 from wasserstein import Cluster
 from PointProcess import PointProcessTrain
 
-#process data for clustering
-def create_data ():
-    '''
-    fields = ['XCOORD', 'YCOORD', 'CALL_TYPE_FINAL_D', 'CALL_TYPE_FINAL', 'DATE_TIME']
-    full_data = pd.read_csv('FixedLatLongTimeLoc_AndTimeSorted.csv', usecols=fields)
-    full_data['DATE_TIME'] =  pd.to_datetime(full_data['DATE_TIME'], format='%Y-%m-%d %H:%M:%S')
-    full_data = full_data.sort_values(by='DATE_TIME')
-    data = full_data[300000:305000]
-    data.reset_index(drop=True, inplace=True)
-    
-    pObject = PointProcessTrain(data)
-    pdata = pObject.test_locs_for_wasserstein()
-    print (pdata)
-    '''
-    pdata = np.load('locs.npy')
-    grid_loc = np.empty((0,0))
-    lst = pdata[:,2].tolist()
-    scom = max(set(lst), key=lst.count)
-    
-    for i in range (len(pdata)):
-        if (pdata [i,2] != scom and pdata [i,2] > 0.0):
-            loc = pdata[i]
-            grid_loc = np.append(grid_loc, loc)
-    grid_loc = grid_loc.reshape ((len(grid_loc) // 3, 3))
-    
-    #print (len(grid_loc))
-    #print (len(pdata) - len(grid_loc))
-    size = np.random.randint(low = 100 , high = 250)
-    points = np.random.choice(np.arange(len(grid_loc)), replace = False, size = size)
-    grid_loc = grid_loc [points]
-    #grid_loc = grid_loc [410:610]
-
-    graph_size = 100
-    grid_loc = create_data()
-    print ('data size: ', len(grid_loc))
-    '''
-    plt.scatter(grid_loc [:,0], grid_loc [:,1])
-    plt.title("Number of events in each grid")
-    plt.show()
-    '''    
-    n = np.random.randint(low = 15, high = 35)
-    print ('cluster size: ', n)
-    cluster = Cluster (grid_loc, n)
-    #d = cluster.driving_distance ([40.730610, -73.935242], [38.889931, -77.009003])
-    
-    #init wasserstein
-    #cluster.process_data(10)
-    lam = cluster.learn_lam(20, True)
-    print (lam)
-    data = cluster.get_data()
-    centers = cluster.get_centers()
-    dist = cluster.get_dist()
-    clusterSizes, clusterVar = cluster.get_cluster_stats()
-
-    plt.title ('Wasserstein')
-    plt.scatter(data[:,0], data[:,1])
-    plt.scatter(centers[:,0], centers[:,1], c = 'red', s = graph_size, alpha = 0.5)
-    plt.show()
-    
-    summary1 = [dist]
-
-    #init wasserstein + round_off combo
-    #cluster.process_data(10)
-    #cluster.process_data_kmeans(True)
-    cluster.round_off()
-    data = cluster.get_data()
-    centers = cluster.get_centers()
-    dist = cluster.get_dist()
-    clusterSizes, clusterVar = cluster.get_cluster_stats()
-
-    plt.title ('Wasserstein + Round_Off')
-    plt.scatter(data[:,0], data[:,1])
-    plt.scatter(centers[:,0], centers[:,1], c = 'red', s = graph_size, alpha = 0.5)
-    plt.show()
-    
-    summary2 = [dist]
-    
-    #init kmeans
-    cluster.process_data_kmeans(False)
-    data = cluster.get_data()
-    centers = cluster.get_centers()
-    dist = cluster.get_dist()
-    clusterSizes, clusterVar = cluster.get_cluster_stats()
-
-    plt.title ('Kmeans')
-    plt.scatter(data[:,0], data[:,1])
-    plt.scatter(centers[:,0], centers[:,1], c = 'red', s = graph_size, alpha = 0.5)
-    plt.show()
-    
-    summary3 = [dist]
-    
-    #return jsonify ([summary1, summary2, summary3])
-    
-    return grid_loc
-
-
 from flask import Flask, redirect, url_for, request, jsonify     
 app = Flask(__name__)
 
@@ -134,28 +38,22 @@ def emergencies():
         time_interval = 15
  
     Date =  datetime.datetime.fromtimestamp(float(start_time))
-    lonmin = -86.4619147125; lonmax =  -85.60543100000002; latmin = 39.587905; latmax = 40.0099;
     total_output = []
-    
-    #for now, generate 100 fake locations        
-    for j in range (int(interval_count)):
-        loc = np.zeros((5,3))
-        for i in range (5):
-            loc [i,0] = np.random.uniform(lonmin, lonmax)
-            loc [i,1] = np.random.uniform(latmin, latmax)
-            loc [i,2] = np.random.uniform(0, 1)
-            
+    sample = np.load ('SampleOutputs.npz')
+    loc = sample['intensities']
+           
+    for j in range (int(interval_count)):            
         output = {
             'start': Date + datetime.timedelta(0,60*time_interval*j),
             'interval_length': time_interval,
             'emergencies': []
         }
-        for i in range (len(loc)):
+        for i in range (len(loc[0])):
             output ['emergencies'].append({
-                'intensity': loc[i,2],
+                'intensity': loc [j][i,2],
                 'location': {
-                    'lat': loc [i,1],
-                    'long': loc [i,0]
+                    'lat': loc [j][i,0],
+                    'long': loc [j][i,1]
                     }
             })
         total_output.append (output)
@@ -164,22 +62,20 @@ def emergencies():
 @app.route('/assignments', methods = ['POST'])
 def assignments():
     if (request.method == 'POST'):
-        lonmin = -86.4619147125; lonmax =  -85.60543100000002; latmin = 39.587905; latmax = 40.0099;
-        em_data = np.zeros((20,2))
 
-        
-        for i in range (len(em_data)):
-            em_data [i,0] = np.random.uniform(lonmin, lonmax)
-            em_data [i,1] = np.random.uniform(latmin, latmax)
-
-            
+        sample = np.load ('SampleOutputs.npz')
+        em_data = sample ['intensities']
+    
         data = request.get_json()
+        
         trucks = filter_data(data ['trucks'])
         start_time = datetime.datetime.fromtimestamp(float(data ['start_time']))
         interval_time = data ['interval_time']
         interval_count = data ['interval_count']
         virtual = trucks [:,2]
-        assignments = dummy_data (trucks, interval_time, interval_count, em_data)
+        
+        #assignments = dummy_data (trucks, interval_time, interval_count, em_data)
+        assignments = wasserstein_cluster (trucks, interval_time, interval_count, em_data)
 
         output = {
             'date': start_time,
@@ -222,7 +118,7 @@ def filter_data (data):
         trucks [i,4] = data [i]['type']
     return trucks
     
-def dummy_data (trucks, interval_time, interval_count, em_data):
+def dummy_data (trucks, interval_time, interval_count, em_data):    
     data = np.zeros((len(trucks),3))
     data[:,0] = trucks [:,0]
     data[:,1] = trucks [:,1]
@@ -235,17 +131,50 @@ def dummy_data (trucks, interval_time, interval_count, em_data):
     centers = kmeans.cluster_centers_
 
     return centers
-   
-'''   
-@app.route('/login', methods = ['POST', 'GET'])
-def login():
-    if (request.method == 'POST'):
-        return redirect(url_for('/assignments'))
-      
-    else:
-        start_time = request.form.get('start_time')
-        interval_count = request.form.get('interval_count')
-        return redirect(url_for('/emergencies', start_time = start_time, interval_count = interval_count))
+
+#practical assignments
 '''
+    em_data is the locs for wasserstein over the given interval
+    2.) Call Cluster in wasserstein passing em_data and len(all trucks with virtual = true)
+    3.) Initialize centers with the locations of the true trucks
+    4.) Cluster the data and return the centers
+'''
+def wasserstein_cluster (trucks, interval_time, interval_count, em_data):
+    grid_loc, data = shrink_data (em_data, interval_count, trucks)
+    data = np.zeros((len(trucks),3))
+
+    cluster = Cluster (grid_loc, len(data))
+    cluster.set_centers (data[:,0:2], len(data))
+    lam = cluster.learn_lam(10, False)
+    centers = cluster.get_centers()
+    data = cluster.get_data()    
+    return centers
+
+def shrink_data (em_data, interval_count, trucks):
+    data = np.zeros((len(trucks),3))
+    data[:,0] = trucks [:,0]
+    data[:,1] = trucks [:,1]
+    data[:,2] = trucks [:,2]
+    data = data.tolist()
+    data = [[lat, long, virtual] for (lat, long, virtual) in data if virtual == True]
+    data = np.array(data)
+
+    em_shrink = sum (em_data [0:interval_count,:])
+    em_shrink [:,0:2] = em_shrink [:,0:2] / interval_count
+    grid_loc = np.empty((0,0))
+    lst = em_shrink[:,2].tolist()
+    scom = max(set(lst), key=lst.count)
+    
+    for i in range (len(em_shrink)):
+        if (em_shrink [i,2] != scom and em_shrink [i,2] > 0.0):
+            loc = em_shrink[i]
+            grid_loc = np.append(grid_loc, loc)
+    grid_loc = grid_loc.reshape ((len(grid_loc) // 3, 3))
+    temp = np.copy(grid_loc [:,0])
+    grid_loc [:,0] = grid_loc [:,1]
+    grid_loc [:,1] = temp
+
+    return grid_loc, data
+
 if __name__ == '__main__':
    app.run()
